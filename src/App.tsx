f2b4ebd5a99da3b2c87a8c17efe50aea2aade109
Hermann Czedik-Eysenberg <git-dev@hermann.czedik.net>
2020-04-05 21:39:07 +0200

    Simple drawing on canvas using mouse

--- src/App.tsx
+++ src/App.tsx
@@ -20,21 +20,50 @@ function App() {
 
   const handleClick = () => toggleToFullscreenAndLandscapeOnMobile();
 
+  function getMousePos(canvas: HTMLCanvasElement, event: React.MouseEvent) {
+    let rect = canvas.getBoundingClientRect(), // abs. size of element
+      scaleX = canvas.width / rect.width,    // relationship bitmap vs. element for X
+      scaleY = canvas.height / rect.height;  // relationship bitmap vs. element for Y
+
+    return {
+      x: (event.clientX - rect.left) * scaleX,   // scale mouse coordinates after they have
+      y: (event.clientY - rect.top) * scaleY     // been adjusted to be relative to element
+    }
+  }
+
+  let pos: { x: number, y: number } | null = null;
+
+  const handleMouseMove = (event: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
+    if (event.buttons !== 1) {
+      pos = null;
+      return;
+    }
+    const canvas = event.currentTarget;
+    const { x, y } = getMousePos(canvas, event);
+    const ctx = canvas.getContext("2d")!;
+    ctx.lineCap = "round";
+    ctx.lineWidth = 10;
+    ctx.beginPath();
+    if (pos === null)
+      pos = { x, y };
+    ctx.moveTo(pos.x, pos.y);
+    ctx.lineTo(x, y);
+    ctx.stroke();
+    pos = { x, y };
+  };
+  const handleMouseOut = () => {
+    pos = null;
+  }
+
+  // TODO touch events: check how I did it for Rufbot
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p onClick={handleClick}>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <canvas width="600" height="600" onMouseDown={handleMouseMove} onMouseMove={handleMouseMove} onMouseOut={handleMouseOut}></canvas>
+        <button onClick={handleClick}>
+          To Fullscreen
+        </button>
       </header>
     </div>
   );
