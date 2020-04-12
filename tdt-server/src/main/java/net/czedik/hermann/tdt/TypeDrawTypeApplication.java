b1f775c9c1b854766efb83765028d38405ac5ab5
Hermann Czedik-Eysenberg <git-dev@hermann.czedik.net>
2020-04-12 19:38:02 +0200

    Add Sprint Boot Java server application

---
+++ tdt-server/src/main/java/net/czedik/hermann/tdt/TypeDrawTypeApplication.java
@@ -0,0 +1,13 @@
+package net.czedik.hermann.tdt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TypeDrawTypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TypeDrawTypeApplication.class, args);
+	}
+
+}
---
+++ tdt-server/src/test/java/net/czedik/hermann/tdt/TypeDrawTypeApplicationTests.java
@@ -0,0 +1,13 @@
+package net.czedik.hermann.tdt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TypeDrawTypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
