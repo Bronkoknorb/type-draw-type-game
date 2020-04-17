547f44342dc721459945127cbcaff39476702dd7
Hermann Czedik-Eysenberg <git-dev@hermann.czedik.net>
2020-04-17 18:27:31 +0200

    Simplify proxy configuration

--- tdt-webapp/src/setupProxy.js
+++ tdt-webapp/src/setupProxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = app => {
-    app.use('/api', createProxyMiddleware('/api', {
+    app.use(createProxyMiddleware('/api/', {
         target: 'http://localhost:8080',
         ws: true,
         logLevel: 'debug'
