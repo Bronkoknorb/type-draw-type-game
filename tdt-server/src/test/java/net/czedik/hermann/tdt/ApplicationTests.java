8c749c8d8105be0c74ad02f2093a35a94cdc9848
Hermann Czedik-Eysenberg <git-dev@hermann.czedik.net>
2020-05-03 00:13:03 +0200

    Solve exception in ApplicationTests

--- tdt-server/src/test/java/net/czedik/hermann/tdt/ApplicationTests.java
+++ tdt-server/src/test/java/net/czedik/hermann/tdt/ApplicationTests.java
@@ -2,8 +2,10 @@ package net.czedik.hermann.tdt;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 @SpringBootTest
+@WebAppConfiguration
 class ApplicationTests {
 
     @Test
