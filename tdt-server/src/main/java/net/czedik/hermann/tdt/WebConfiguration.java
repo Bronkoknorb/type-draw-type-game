140398f8cc1878ec011d7529bd50f019d93a7fe9
Hermann Czedik-Eysenberg <git-dev@hermann.czedik.net>
2020-05-19 21:47:05 +0200

    Configure correct caching and enable compression

--- tdt-server/src/main/java/net/czedik/hermann/tdt/WebConfiguration.java
+++ tdt-server/src/main/java/net/czedik/hermann/tdt/WebConfiguration.java
@@ -1,8 +1,12 @@
 package net.czedik.hermann.tdt;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
@@ -11,5 +15,18 @@ public class WebConfiguration implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/g/{gameId:\\w+}").setViewName("forward:/");
         registry.addViewController("/new").setViewName("forward:/");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // very long caching for static (hashed files)
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/public/static/")
+                .setCacheControl(CacheControl.maxAge(365, TimeUnit.DAYS));
+
+        // no caching (browser needs to re-validate) for all the rest
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/public/")
+                .setCacheControl(CacheControl.noCache());
     }
 }
