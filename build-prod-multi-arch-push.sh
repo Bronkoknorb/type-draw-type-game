eea23e954030d1280617dda3775000452dec4ae2
Hermann Czedik-Eysenberg <git-dev@hermann.czedik.net>
2020-08-16 17:41:39 +0200

    Use correct Dockerfile

--- build-prod-multi-arch-push.sh
+++ build-prod-multi-arch-push.sh
@@ -10,4 +10,5 @@ docker buildx build \
               --platform linux/amd64,linux/arm/v7,linux/arm64 \
               --tag bronkoknorb/type-draw-type-game:latest \
               --push \
+              -f Dockerfile_prod \
               .
