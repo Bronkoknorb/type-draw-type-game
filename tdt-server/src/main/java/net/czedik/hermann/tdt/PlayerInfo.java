2d9cc43f9e2ee3d2f7c1f20ea3079639eee82476
Hermann Czedik-Eysenberg <git-dev@hermann.czedik.net>
2023-05-06 20:15:02 +0200

    Use record

--- tdt-server/src/main/java/net/czedik/hermann/tdt/PlayerInfo.java
+++ tdt-server/src/main/java/net/czedik/hermann/tdt/PlayerInfo.java
@@ -2,17 +2,12 @@ package net.czedik.hermann.tdt;
 
 import java.util.Objects;
 
-public class PlayerInfo {
+public record PlayerInfo(String name, String face, boolean isCreator) {
     // on purpose this one does not include the userId, because it goes to the
     // frontend (for all players) and the userId should be secret
 
-    public final String name;
-    public final String face;
-    public final boolean isCreator;
-
-    public PlayerInfo(String name, String face, boolean isCreator) {
-        this.name = Objects.requireNonNull(name);
-        this.face = Objects.requireNonNull(face);
-        this.isCreator = isCreator;
+    public PlayerInfo {
+        Objects.requireNonNull(name);
+        Objects.requireNonNull(face);
     }
 }
