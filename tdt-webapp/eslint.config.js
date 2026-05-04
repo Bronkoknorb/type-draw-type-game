befaaa0894a704b8adf7c5c8815ff90763163044
Hermann Czedik-Eysenberg <git-dev@hermann.czedik.net>
2026-05-04 22:31:44 +0200

    Ignore linting issues in generated files

--- tdt-webapp/eslint.config.js
+++ tdt-webapp/eslint.config.js
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', '.yarn', '.pnp.cjs', '.pnp.loader.mjs']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
