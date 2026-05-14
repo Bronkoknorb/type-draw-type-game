9dfb0e33c9b12e7c2336f229a6af032d176df4c4
Hermann Czedik-Eysenberg <git-dev@hermann.czedik.net>
2026-05-14 11:32:30 +0200

    Add @eslint-react/eslint-plugin

--- tdt-webapp/eslint.config.js
+++ tdt-webapp/eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import eslintReact from "@eslint-react/eslint-plugin";
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
@@ -14,10 +15,18 @@ export default defineConfig([
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      eslintReact.configs["strict-type-checked"],
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      // Use TypeScript ESLint parser for TypeScript files
+      parser: tseslint.parser,
+      parserOptions: {
+        // Enable project service for better TypeScript integration
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
   },
 ])
